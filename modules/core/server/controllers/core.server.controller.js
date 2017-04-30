ccc150bde41416df059d313522a9246f8c34927e
s0j0hn <contact@jansocha.com>
2017-04-30 19:40:42 +0200

    fix for bans filter on core

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -1,38 +1,13 @@
 'use strict';
-var ipaddr = require('ipaddr.js'),
-    iplib = require('ip'),
-    mongoose = require('mongoose'),
-    validator = require('validator'),
-    Ban = mongoose.model('Ban'),
+var validator = require('validator'),
     path = require('path'),
     config = require(path.resolve('./config/config'));
 
-function getIP(req) {
-    var ipString = (req.headers['X-Forwarded-For'] ||
-        req.headers['x-forwarded-for'] ||
-        '').split(',')[0] ||
-        req.connection.remoteAddress;
 
-    if (ipaddr.isValid(ipString)) {
-        try {
-            var addr = ipaddr.parse(ipString);
-            if (ipaddr.IPv6.isValid(ipString) && addr.isIPv4MappedAddress()) {
-                return addr.toIPv4Address().toString();
-            }
-            return addr.toNormalizedString();
-        } catch (e) {
-            return ipString;
-        }
-    }
-    return 'unknown';
-}
 /**
  * Render the main application page
  */
 exports.renderIndex = function (req, res) {
-    var ip = getIP(req);
-    var banned = false;
-
     var safeUserObject = null;
     if (req.user) {
         safeUserObject = {
@@ -55,25 +30,10 @@ exports.renderIndex = function (req, res) {
         };
     }
 
-    Ban.find().sort('created').exec(function(err, bans) {
-        for (var i = 0; i < bans.length; i++) {
-            var ban = bans[i];
-            if ( ban.address && iplib.cidrSubnet(ban.address).contains(ip)){
-                banned = true;
-            }
-        }
-        if (banned) {
-            res.status(422).render('modules/core/server/views/banned', {
-                ip: ip
-            });
-        } else {
-            res.render('modules/core/server/views/index', {
-                user: JSON.stringify(safeUserObject),
-                sharedConfig: JSON.stringify(config.shared)
-            });
-        }
+    res.render('modules/core/server/views/index', {
+        user: JSON.stringify(safeUserObject),
+        sharedConfig: JSON.stringify(config.shared)
     });
-
 
 };
 
