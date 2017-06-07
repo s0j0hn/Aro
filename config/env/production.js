f6fab3ad7a25ef24524721667055602589e80d15
jan222 <159030@supinfo.com>
2017-06-07 19:04:04 +0200

    fixes

--- config/env/production.js
+++ config/env/production.js
@@ -9,9 +9,9 @@ module.exports = {
     },
     port: process.env.PORT || 443,
     db: {
-        uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/meanProd',
+        uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/pjtProd',
         options: {
-            user: 'janMean',
+            user: '',
             pass: ''
         },
         // Enable mongoose debug mode
