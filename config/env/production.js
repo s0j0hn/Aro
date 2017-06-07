b96834db6034580cc0ce5a7bf4845f2e8d25ce3d
jan222 <159030@supinfo.com>
2017-06-07 19:03:29 +0200

    fixes

--- config/env/production.js
+++ config/env/production.js
@@ -12,7 +12,7 @@ module.exports = {
         uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/meanProd',
         options: {
             user: 'janMean',
-            pass: '05021994Jan!'
+            pass: ''
         },
         // Enable mongoose debug mode
         debug: process.env.MONGODB_DEBUG || false
